50f0d6dbde6233f7428cf84556f94a8a2da9edb2
Joe Oketch <wokirijoe@gmail.com>
2020-11-29 06:13:21 +0300

    Modified requirements

--- herehacksproject/settings.py
+++ herehacksproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,4 +147,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "templates", "prod")
 ]
 
+
+django_heroku.settings(locals())
 
