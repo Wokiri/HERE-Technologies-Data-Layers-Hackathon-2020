fe928c69b94c4ad81191ee4fd3d9902be7a80ab1
Joe Oketch <wokirijoe@gmail.com>
2020-11-29 05:39:56 +0300

    Modified STATIC_ROOT in settings

--- herehacksproject/settings.py
+++ herehacksproject/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
 STATIC_URL = '/static/'
 
 # static files (e.g. images, JavaScript, CSS).
@@ -145,4 +147,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "templates", "prod")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
